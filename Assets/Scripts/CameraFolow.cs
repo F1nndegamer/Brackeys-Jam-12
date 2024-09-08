dac369f2008b0c57a233f608357a907516745878
F1nndegamer <finnnijpels2@gmail.com>
2024-09-08 17:50:45 +0200

    update scene

--- Assets/Scripts/CameraFolow.cs
+++ Assets/Scripts/CameraFolow.cs
@@ -13,11 +13,7 @@ public class CameraFollow : MonoBehaviour
 
         // Smoothly interpolate between the camera's current position and the desired position
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-
-        // Lock the Z position while keeping the X and Y positions smooth
         transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
-
-        // Optionally, keep the camera from rotating
         transform.rotation = Quaternion.identity;
     }
 }
