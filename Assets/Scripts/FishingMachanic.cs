3a3d348fdf2e45f02ce08300bf61326bb384af87
icantdo <80340838+icantdo@users.noreply.github.com>
2024-09-08 20:14:35 +0300

    fishing demo update

--- Assets/Scripts/FishingMachanic.cs
+++ Assets/Scripts/FishingMachanic.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class FishingMachanic : MonoBehaviour
 {
@@ -9,22 +10,29 @@ public class FishingMachanic : MonoBehaviour
     [SerializeField] private float timer;
     [SerializeField] private float timerMulti;
     [SerializeField] private float fishSpeed;
-    [SerializeField] private Slider fish;
+    [SerializeField] private UnityEngine.UI.Slider fish;
+    private RectTransform sliderHandleRect;
 
     [Header("Fishrod")]
     [SerializeField] private float fishrodSpeed;
     [SerializeField] private Scrollbar fishrode;
+    private RectTransform scrollbarHandleRect;
 
     public bool isFishing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sliderHandleRect = fish.handleRect;
+        scrollbarHandleRect = fishrode.handleRect;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (AreHandlesTouching(sliderHandleRect, scrollbarHandleRect))
+        {
+            Debug.Log("aaaa");
+        }
         BaseFishingMach();
         FishMachanic();
         FishrodMachanic();
@@ -62,5 +70,21 @@ public class FishingMachanic : MonoBehaviour
         {
             fishrode.value += -fishrodSpeed * 1.5f;
         }
+    }
+    bool AreHandlesTouching(RectTransform rect1, RectTransform rect2)
+    {
+        Rect rect1Bounds = RectTransformToScreenSpace(rect1);
+        Rect rect2Bounds = RectTransformToScreenSpace(rect2);
+
+        return rect1Bounds.Overlaps(rect2Bounds);
+    }
+    Rect RectTransformToScreenSpace(RectTransform rectTransform)
+    {
+        Vector3[] worldCorners = new Vector3[4];
+        rectTransform.GetWorldCorners(worldCorners);
+
+        return new Rect(worldCorners[0].x, worldCorners[0].y,
+                        worldCorners[2].x - worldCorners[0].x,
+                        worldCorners[2].y - worldCorners[0].y);
     }
 }
