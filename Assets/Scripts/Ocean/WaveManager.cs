7416abf546249a591125ed74d539785b6169a085
Colby-O <okeefeca@icloud.com>
2024-09-08 23:13:54 -0400

    Error Fix

--- Assets/Scripts/Ocean/WaveManager.cs
+++ Assets/Scripts/Ocean/WaveManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveManger : MonoBehaviour
+public class WaveManager : MonoBehaviour
 {
 
     [Header("References")]
