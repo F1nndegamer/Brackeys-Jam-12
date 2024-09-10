9d3b018ddb01f63113cce20363f66c48c01dd073
icantdo <80340838+icantdo@users.noreply.github.com>
2024-09-10 13:28:05 +0300

    fish minigame

--- Assets/Scripts/FishingMachanic.cs
+++ Assets/Scripts/FishingMachanic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,35 +7,51 @@ using UnityEngine.UI;
 
 public class FishingMachanic : MonoBehaviour
 {
+    int y = 1;
     [SerializeField] private List<Fish> fishList;
-    private float timer = 3;
-    public bool isFishing = false;
+    public bool isFishing = false; 
+    Fish randomFish;
+    public GameObject bar;
     public static int fishrode;
     public static string lastFishCaughtName;
+    Transform greenBar;
+    Transform whiteBar; 
+    private void Start()
+    {
+        greenBar = bar.transform.GetChild(0);
+        whiteBar = bar.transform.GetChild(1);
+    }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isFishing)
         {
-            isFishing = !isFishing;
+            randomFish = fishList[UnityEngine.Random.Range(0, fishList.Count)];
+            isFishing = true;
         }
-        Catching();
-    }
-    void Catching()
-    {
-        if (!isFishing) return;
-        Fish randomFish = fishList[Random.Range(0, fishList.Count)];
-        timer -= Time.deltaTime;
-        if (timer < 0)
+        if (Input.GetMouseButtonDown(1) && isFishing)
         {
-            Debug.Log("catch");
-            timer = (Random.value + 0.1f) * FishingDifficulty(randomFish.difficulty, fishrode); 
-            Player.Instance.UpdateMoney(randomFish.price);
-            lastFishCaughtName = randomFish.name;
+            Catching(randomFish);
             isFishing = false;
+        }
+        if (isFishing)
+        {
+            bar.SetActive(true);
+            WhiteBarMove(1);
         }
         else
         {
-            Debug.Log("waiting");
+            bar.SetActive(false);
+        }
+    }
+    void Catching(Fish randfish)
+    {
+        if (!isFishing) return;
+        float x = Math.Abs(randfish.difficulty - 100f) / 100;
+        greenBar.localScale = new Vector2(x, 1);
+        if (whiteBar.localPosition.x > -(greenBar.localScale.x / 2 - 0.03f) && whiteBar.localPosition.x < (greenBar.localScale.x / 2 - 0.03f))
+        {
+            //Player.Instance.UpdateMoney(randfish.price);
+            lastFishCaughtName = randfish.name;
         }
     }
     public int FishingDifficulty(int fishsdif, int fisrodebuff = 0)
@@ -42,5 +59,17 @@ public class FishingMachanic : MonoBehaviour
         int res = 0;
         res = fishsdif - fisrodebuff;
         return res;
+    }
+    void BarMechanic(Fish selectFish)
+    {
+
+    }
+    void WhiteBarMove(float whiteBarSpeed)
+    {
+        if (whiteBar.localPosition.x >= 0.5f || whiteBar.localPosition.x <= -0.5f)
+        {
+            y *= -1;
+        }
+        whiteBar.localPosition = new Vector2(whiteBar.localPosition.x + whiteBarSpeed * y * Time.deltaTime, 0);
     }
 }
