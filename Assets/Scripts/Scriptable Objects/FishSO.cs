ab928d85ab4cb9fdb118f6572115caed3f248e96
codebender <khoa28122005@gmail.com>
2024-09-10 19:28:11 +0700

    Add FishSO

---
+++ Assets/Scripts/Scriptable Objects/FishSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class FishSO : ScriptableObject
+{
+    public Sprite icon;
+    public string fishName;
+    public int difficulty;
+    public int price;
+}
