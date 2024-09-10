dc8d87934c636ead8f43312a8b5033ce82572dc5
codebender <khoa28122005@gmail.com>
2024-09-10 17:43:55 +0700

    Update Main Menu UI Fonts

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button creditsButton;
     private void Start()
     {
         playButton.onClick.AddListener(() =>
         {
             Loader.Load(Loader.Scene.GameScene);
         });
-    }
-    public void PlayButton()
-    {
-        Loader.Load(Loader.Scene.GameScene);
+        optionsButton.onClick.AddListener(() =>
+        {
+
+        });
+        creditsButton.onClick.AddListener(() =>
+        {
+
+        });
+
     }
 }
