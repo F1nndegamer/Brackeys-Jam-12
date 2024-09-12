979cf828f76742dc2198247d47ac32873f93784e
icantdo <80340838+icantdo@users.noreply.github.com>
2024-09-12 16:40:42 +0300

    storm particle system

--- Assets/Scripts/Storm.cs
+++ Assets/Scripts/Storm.cs
@@ -9,23 +9,33 @@ public class StormManager : MonoBehaviour
     public float stormSpeed = 5f; // Speed of the storm
     public float outrunDistance = 20f; // Distance the player needs to be ahead of the storm to survive
     public GameObject currentStorm;
-
+    public ParticleSystem particleSystem;
+    private float alpha = 0.11f;
+    private int x;
     private void Start()
     {
+        ;
         StartCoroutine(StormRoutine());
     }
 
     private IEnumerator StormRoutine()
     {
+        var mainModule = particleSystem.main;
         while (true)
         {
-            yield return new WaitForSeconds(spawnInterval);
+            Color color1 = new Color(1f , 1f , 1f, alpha);
+            Color color2 = new Color(0.82f , 0.82f , 0.82f, alpha);
+            mainModule.startColor = new ParticleSystem.MinMaxGradient(color1,color2);
+            alpha += 0.01f;
+            yield return new WaitForSeconds(1);
             SpawnStorm();
         }
     }
 
     void SpawnStorm()
     {
+        x++;
+        if (x != spawnInterval) return;
         if (currentStorm == null)
         {
             Vector3 spawnPosition = player.position + new Vector3(outrunDistance, 0, -1);
@@ -52,5 +62,6 @@ public class StormManager : MonoBehaviour
     {
         Debug.Log("Player has been caught by the storm! Game over.");
         Destroy(currentStorm);
+        alpha = 0.11f;
     }
 }
