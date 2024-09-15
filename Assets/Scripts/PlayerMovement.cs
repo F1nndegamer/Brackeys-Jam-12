257fcd535726e797822e6889217731bc965249a0
codebender <khoa28122005@gmail.com>
2024-09-15 16:05:10 +0700

    Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,16 +10,16 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 0.2f;
+    [SerializeField] private Animator animator;
 
     private Vector2 movement;
-    private Animator animator;
-    private string currentAnimationName;
+    private string currentAnimationName = "Idle";
 
     void Start()
     {
-        animator = GetComponent<Animator>();
         if (rb == null) rb = GetComponent<Rigidbody2D>();
         if (fishingMachanic == null) fishingMachanic = GetComponent<FishingMachanic>();
+        ChangeAnimation("Idle");
     }
 
     void Update()
@@ -31,14 +32,15 @@ public class PlayerMovement : MonoBehaviour
         // Rotate the player based on movement direction
         if (movement != Vector2.zero)
         {
-            ChangeAnimation("Sail");
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle), rotateSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle-90), rotateSpeed * Time.deltaTime);
+            ChangeAnimation("Sail");
         }
         else
         {
             ChangeAnimation("Idle");
         }
+        Debug.Log(currentAnimationName);
     }
 
     void FixedUpdate()
@@ -54,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (currentAnimationName != animationName)
         {
-            animator.ResetTrigger(animationName);
+            animator.ResetTrigger(currentAnimationName);
             currentAnimationName = animationName;
             animator.SetTrigger(currentAnimationName);
         }
