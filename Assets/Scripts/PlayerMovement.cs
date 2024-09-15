7ee7dc3c29c39ede2f05a63db67bfb994dac5d23
codebender <khoa28122005@gmail.com>
2024-09-15 15:27:25 +0700

    Balance Fish Price And Max Fishing Distance

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotateSpeed = 0.2f;
 
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite[] directionSprites;
+
     private Vector2 movement;
+    private Animator animator;
+    private string currentAnimationName;
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         if (rb == null) rb = GetComponent<Rigidbody2D>();
         if (fishingMachanic == null) fishingMachanic = GetComponent<FishingMachanic>();
     }
@@ -28,8 +34,13 @@ public class PlayerMovement : MonoBehaviour
         // Rotate the player based on movement direction
         if (movement != Vector2.zero)
         {
+            ChangeAnimation("Sail");
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, angle), rotateSpeed * Time.deltaTime);
+        }
+        else
+        {
+            ChangeAnimation("Idle");
         }
     }
 
@@ -41,5 +52,14 @@ public class PlayerMovement : MonoBehaviour
     public void IncreaseSpeed(float amount)
     {
         moveSpeed += amount;
+    }
+    private void ChangeAnimation(string animationName)
+    {
+        if (currentAnimationName != animationName)
+        {
+            animator.ResetTrigger(animationName);
+            currentAnimationName = animationName;
+            animator.SetTrigger(currentAnimationName);
+        }
     }
 }
